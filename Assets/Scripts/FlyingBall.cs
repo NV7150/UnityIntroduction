06283524eb07dc861e74962e4152f41f96000192
NV7150 <7150macNV777DAI@gmail.com>
2019-09-04 16:15:22 +0900

    version1

---
+++ Assets/Scripts/FlyingBall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlyingBall : MonoBehaviour {
+    [SerializeField] private Text _text;
+    private Rigidbody _rigidbody;
+    // Start is called before the first frame update
+    void Start() {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(transform.right * -1000f + transform.up * 500f);
+    }
+
+    // Update is called once per frame
+    void Update(){
+    }
+}
